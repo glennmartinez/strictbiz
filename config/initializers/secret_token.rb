f86f4237c28f688227f985316dc64651108185f2
Glenn Martin <glennmartin76@gmail.com>
2012-08-18 20:03:07 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Strictbiz::Application.config.secret_token = 'b030e2313f49b0cb81bde2c83062c6c6a623c58f55eb41318329aee7ac09a07fc2cb82f2564a1a8255f0c58d5dc579e356b1c54a7e78e919876aa1a9d7968da1'
